fb6510196876ce57fe9d790b80e484a440cb856e
unknown <nsaedae@gmail.com>
2022-01-13 11:40:22 +0900

    2022/01/13 수업예제

---
+++ Study/src/ch01/Test.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 int x=5, y=0, z=0;
+		 y = x++;
+		 z = --x;
+		 System.out.println(x + ", " + y + ", " + z);
+	}
+}
---
+++ Study/src/ch12/Sub3_UpdateTest.java
@@ -0,0 +1,45 @@
+package ch12;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * 날짜 : 2022/01/13
+ * 이름 : 김철학
+ * 내용 : 자바 JDBC Update 실습하기 교재 p557 
+ */
+public class Sub3_UpdateTest {
+	public static void main(String[] args) {
+		
+		// DB정보
+		final String HOST = "jdbc:mysql://54.180.160.240:3306/test";
+		final String USER = "test";
+		final String PASS = "1234";
+		
+		try {
+			// 1단계 - JDBC 드라이버 로드
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			// 2단계 - 데이터베이스 접속
+			Connection conn = DriverManager.getConnection(HOST, USER, PASS);
+			
+			// 3단계 - SQL실행 객체생성(Statement)
+			Statement stmt = conn.createStatement();
+			
+			// 4단계 - SQL실행
+			String sql = "UPDATE `User1` SET `age` = `age` + 1 WHERE `uid`='P101';";     
+			int count = stmt.executeUpdate(sql);
+			
+			System.out.println(count+"개의 데이터가 수정 되었습니다.");
+			
+			// 5단계 - 결과처리(SELECT일 경우)
+			// 6단계 - 데이터베이스 종료
+			conn.close();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Update 완료...");
+	}
+}
